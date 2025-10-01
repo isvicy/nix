2ad0f5ef091e0a96a6c235204d61d21ec64430a9
isvicy <isregistermail@gmail.com>
2025-10-02 04:00:20 +0800

    feat: use more binary cache from multi source

--- modules/system.nix
+++ modules/system.nix
@@ -8,6 +8,20 @@
   nix.settings = {
     trusted-users = [username];
     experimental-features = "nix-command flakes";
+    substituters = [
+      # high priority since it's almost always used
+      "https://cache.nixos.org?priority=10"
+
+      "https://niri.cachix.org"
+      "https://nix-community.cachix.org"
+    ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+
+      "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
   nix.gc = {
     automatic = true;
@@ -23,10 +37,12 @@
     isNormalUser = true;
     shell = pkgs.zsh;
     extraGroups = ["wheel" "docker"];
+    packages = [pkgs.gnupg];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBZD8Fl36zcqyYut8vu2Vhcv8m+JcxNhiRDrtM3hxzrv matexpro"
     ];
   };
+  programs.gnupg.agent.enable = true;
   security.sudo.extraRules = [
     {
       users = [username];
