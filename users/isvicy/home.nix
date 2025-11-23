5a88f4ab8a63467ea44ea232056715b0347238b8
isvicy <isregistermail@gmail.com>
2025-11-23 18:36:55 +0800

    refactor: restructure git and delta configuration
    - Moved delta configuration to programs.delta module
    - Enabled git integration explicitly with enableGitIntegration
    - Changed git.userName/userEmail to git.settings.user.name/email
      following the proper Home Manager configuration structure

--- users/isvicy/home.nix
+++ users/isvicy/home.nix
@@ -11,14 +11,19 @@
     git = {
       enable = true;
       lfs.enable = true;
-      delta.enable = true;
-      delta.options = {
+      settings.user = {
+        name = "isvicy";
+        email = "isregistermail@gmail.com";
+      };
+    };
+    delta = {
+      enable = true;
+      enableGitIntegration = true;
+      options = {
         line-numbers = true;
         side-by-side = true;
         navigate = true;
       };
-      userName = "isvicy";
-      userEmail = "isregistermail@gmail.com";
     };
   };
 }
