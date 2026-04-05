c9a86b97e81dc2adb98ca90392e6cb4fea10e406
isvicy <isvicy@example.com>
2026-04-05 14:57:22 +0800

    fix(home): make ghostty conditional on Linux (not in nixpkgs for darwin)

--- home/programs/terminal.nix
+++ home/programs/terminal.nix
@@ -1,10 +1,13 @@
 {
-  config,
   pkgs,
+  lib,
   ...
 }: {
-  home.packages = with pkgs; [
-    kitty
-    ghostty
-  ];
+  home.packages = with pkgs;
+    [
+      kitty
+    ]
+    ++ lib.optionals pkgs.stdenv.isLinux [
+      ghostty
+    ];
 }
