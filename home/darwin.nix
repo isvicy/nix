c53b67442a922981ef0189d34b0a16e9326c49ea
yangkai <yangkai@msh.team>
2026-04-06 14:08:22 +0800

    feat(darwin): patch library path for building codesnap.nvim

--- home/darwin.nix
+++ home/darwin.nix
@@ -1,4 +1,4 @@
-{username, ...}: {
+{username, pkgs, lib, ...}: {
   imports = [
     ./base.nix
     ./programs/common.nix
@@ -9,6 +9,10 @@
     inherit username;
     homeDirectory = "/Users/${username}";
     stateVersion = "25.11";
+
+    sessionVariables = {
+      LIBRARY_PATH = "${lib.makeLibraryPath [pkgs.libiconv]}\${LIBRARY_PATH:+:$LIBRARY_PATH}";
+    };
   };
 
   programs.home-manager.enable = true;
